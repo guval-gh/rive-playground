c7ff94238af7ad4277249e803e7c3e74ea4cecfe
guval <177042063+guval-gh@users.noreply.github.com>
2024-11-15 15:12:19 +0100

    🔊 Add log on Home example

--- app/index.tsx
+++ app/index.tsx
@@ -5,5 +5,41 @@ import { RiveAnimation } from "@/components/RiveAnimation";
 import Rating from "@/assets/animations/rating.riv";
 
 export default function Playground() {
-  return <RiveAnimation fit={Fit.FitWidth} source={Rating} autoplay={true} />;
+  return (
+    <RiveAnimation
+      fit={Fit.FitWidth}
+      source={Rating}
+      autoplay={true}
+      onPlay={(animationName, isStateMachine) => {
+        console.log(
+          "onPlay: ",
+          animationName,
+          "isStateMachine: ",
+          isStateMachine
+        );
+      }}
+      onPause={(animationName, isStateMachine) => {
+        console.log(
+          "onPause:",
+          animationName,
+          "isStateMachine: ",
+          isStateMachine
+        );
+      }}
+      onStop={(animationName, isStateMachine) => {
+        console.log(
+          "onStop: ",
+          animationName,
+          "isStateMachine: ",
+          isStateMachine
+        );
+      }}
+      onLoopEnd={(animationName, loopMode) => {
+        console.log("onLoopEnd: ", animationName, "loopMode: ", loopMode);
+      }}
+      onRiveEventReceived={(event) => {
+        console.log("onRiveEventReceived: ", event);
+      }}
+    />
+  );
 }
